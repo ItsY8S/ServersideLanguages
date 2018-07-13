952ccb9433203d1ed23ec35f649f7452324c431e
ItsY8S <gri.y8s@gmail.com>
2018-07-13 04:05:29 -0400

    Added Bootstrap and navigation

--- ssl/myframework/views/header.php
+++ ssl/myframework/views/header.php
@@ -1,3 +1,3 @@
-<link href="https://github.com/necolas/normalize.css/blob/master/normalize.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="../assets/css/style.css" rel="stylesheet" />
 
--- ssl/myframework/views/navigation.php
+++ ssl/myframework/views/navigation.php
@@ -1,22 +1,29 @@
 <?php
 
-echo "<nav>";
-echo "<ul>";
+// echo "<nav>";
+// echo "<ul>";
+// foreach($data as $menuTitle) {
+//     echo "<li><a href='/" . $menuTitle . "'>" . $menuTitle . "</a></li>";
+// }
+// echo "</ul>";
+// echo "</nav>";
+
+
+echo "<nav class='navbar navbar-expand-lg navbar-light bg-light'>";
+echo "<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>";
+echo "<span class='navbar-toggler-icon'></span>";
+echo "</button>";
+echo "<div class='collapse navbar-collapse' id='navbarNav'>";
+echo "<ul class='navbar-nav'>";
+
 foreach($data as $menuTitle) {
-
-    if($current == 'home') {
-        echo "<li><a class='" . $menuTitle . "'" . "href='/" . $menuTitle . "'>" . $menuTitle . "</a></li>";
-    }
-    elseif($current == 'api') {
-        echo "<li><a class='" . $menuTitle . "'" . "href='/" . $menuTitle . "'>" . $menuTitle . "</a></li>";
-    }
-    elseif($current == 'crud') {
-        echo "<li><a class='" . $menuTitle . "'" . "href='/" . $menuTitle . "'>" . $menuTitle . "</a></li>";
-    }
-
-    // echo "<li><a href='/" . $menuTitle . "'>" . $menuTitle . "</a></li>";
+    echo "<li class='nav-item'>";
+    echo "<a class='nav-link' href='/" . $menuTitle . "'>" . $menuTitle . "</a>";
+    echo "</li>";
 }
+
 echo "</ul>";
+echo "</div>";
 echo "</nav>";
 
 ?>
